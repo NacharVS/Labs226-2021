dc88cfd80f1858d7dbf4e7b647bd379aa50ff7f7
NikitaMatv <kuzmenko_ka@mail.ru>
2021-09-13 17:33:21 +0300

    Task2.3
    Task 3 leson2

---
+++ Labs226-2021/Task2.3.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Выберете действие + - * /");
+            Console.WriteLine(" +  = 1 " +
+                "- = 2 " +
+                "* = 3" +
+                "/ = 4");
+
+
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите певое чилов");
+            int c = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите вторе чило");
+            int d = int.Parse(Console.ReadLine());
+            int r = 0;
+            if (a == 1) { r = c + d; Console.WriteLine(r); }
+            else if (a == 2) { r = c - d; Console.WriteLine(r); }
+            else if (a == 3) { r = c * d; Console.WriteLine(r); }
+            else if (a == 4) { r = c / d; Console.WriteLine(r); }
+            else { Console.WriteLine("Выберети верное заначение действия"); }
+        }
+
+    }
+}
