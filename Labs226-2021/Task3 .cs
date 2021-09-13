289fa08517114c65aba2cf435c2b9a95c42ce52e
NikitaMatv <kuzmenko_ka@mail.ru>
2021-09-13 17:20:21 +0300

    Task3
    Task3 leson1

---
+++ Labs226-2021/Task3 .cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Labs226_2021
+{
+    class Program
+    {
+        static void Task3(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int c = 1;
+            Double S = 0;
+            Double P = 0;
+            Double p = 0;
+            Double V = 0;
+            S = a * b;
+            P = 2 * (a + b);
+            p = a + b;
+            V = a * b * c;
+            Console.WriteLine(S);
+            Console.WriteLine(P);
+            Console.WriteLine(p);
+            Console.WriteLine(V);
+
+        }
+    }
+}
