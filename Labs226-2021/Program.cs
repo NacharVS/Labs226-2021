c9f14496fba22b53866d712bbb07cd1e3e834581
r3dinc <r3dinc@yandex.ru>
2021-09-13 11:49:27 +0300

    created project

---
+++ Labs226-2021/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Labs226_2021
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
