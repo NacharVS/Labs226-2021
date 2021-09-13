3f1c7c0670b9cdca549bb6cac8bd33a474eb083a
NikitaMatv <kuzmenko_ka@mail.ru>
2021-09-13 17:51:22 +0300

    Task4.2
    Task 2 leson 4

---
+++ Labs226-2021/Task4.2 .cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ConsoleApp2 { }
+class Program
+{
+    static void Main(string[] args)
+    {
+        long a = int.Parse(Console.ReadLine());
+        long b = 0;
+        while (a > 0)
+        {
+            b = b * 10 + a % 10;
+            a = a / 10;
+        }
+        
+            
+        Console.WriteLine(b);
+
+
+
+    }
+}
+
