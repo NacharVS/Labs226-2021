42eace18d1749e658342808b8f845fd3abe03360
NikitaMatv <kuzmenko_ka@mail.ru>
2021-09-13 17:46:55 +0300

    Task4.1
    Task1 leson 4

---
+++ Labs226-2021/Task4.1 .cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ConsoleApp2 { }
+class Program
+{
+    static void Main(string[] args)
+    {
+        int a = int.Parse(Console.ReadLine());
+        int b = int.Parse(Console.ReadLine());
+        int d = a;
+        if (b > 0)
+        {
+            d = d * a;
+            b--;
+        }
+        Console.WriteLine(d);
+
+
+
+    }
+}
+
