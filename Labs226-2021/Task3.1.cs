774ff869bd9a214f82ae4838fd6299bbf9e0d9a6
NikitaMatv <kuzmenko_ka@mail.ru>
2021-09-13 17:36:01 +0300

    Task3.1
    Task1 leson 3

---
+++ Labs226-2021/Task3.1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace ConsoleApp2 { }
+class Program
+{
+    static void Main(string[] args)
+    {
+        int sum = int.Parse(Console.ReadLine());
+
+
+        for (int i = 0; i < 10; i++)
+        {
+            for (int j = 0; j < 10; j++)
+            {
+                for (int k = 0; k < 10; k++)
+                {
+
+                    if (i + j + k == sum)
+                    {
+                        if (i != j)
+                        {
+                            if (j != k)
+                            {
+
+
+                                Console.WriteLine($"{ i}{ j}{ k}");
+                            }
+                        }
+
+                    }
+
+                }
+            }
+
+
+        }
+    }
+}
