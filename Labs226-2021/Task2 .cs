f38571c510fddeab8dc50fca5acac426229e2336
NikitaMatv <kuzmenko_ka@mail.ru>
2021-09-13 17:12:44 +0300

    Task2
    Task2 leson1

---
+++ Labs226-2021/Task2 .cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Labs226_2021
+{
+    class Program
+    {
+        static void Task2(string[] args)
+        {
+            int r = int.Parse(Console.ReadLine());
+            Double Pi = 3.14;
+            Double S = 0;
+            S = 4 * Pi * (r*r);
+            Console.WriteLine(S);
+            
+        }
+    }
+}
