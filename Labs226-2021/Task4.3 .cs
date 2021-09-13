80346af3e341ba728fa5c11f74080c4814c16277
NikitaMatv <kuzmenko_ka@mail.ru>
2021-09-13 18:15:17 +0300

    Task4.3
    task3 leson 4

---
+++ Labs226-2021/Task4.3 .cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int a = int.Parse(Console.ReadLine());
+        int b = int.Parse(Console.ReadLine());
+        int d = 0;
+        int c = 0;
+        int h = 0;
+        while (a > 0)
+        {
+            c = a % 10;
+            if (c != b)
+            {
+            d = d * 10 + a % 10;
+            }
+            a = a / 10;
+        }
+        while (d > 0)
+        {
+            h = h * 10 + d % 10;
+            d = d / 10;
+        }
+        Console.WriteLine(c);
+    }
+}
+
