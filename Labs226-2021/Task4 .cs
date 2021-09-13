200562f104c73ef5b372066e06305b61f57d7386
NikitaMatv <kuzmenko_ka@mail.ru>
2021-09-13 17:25:12 +0300

    Task4
    Task4 leson 1

---
+++ Labs226-2021/Task4 .cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Labs226_2021
+{
+    class Program
+    {
+        static void Task4(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            double c = 0;
+            c = Math.Sqrt(a * a + b * b);
+            Console.WriteLine(c);
+
+        }
+    }
+}
