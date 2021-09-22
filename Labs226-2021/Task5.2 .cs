5ff5b8000f820a39a0303c2d9dfc21cfc5646c50
NikitaMatv <kuzmenko_ka@mail.ru>
2021-09-22 20:20:08 +0300

    Leson 5 task 2

---
+++ Labs226-2021/Task5.2 .cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ConsoleApp4
+{
+    class Program52
+    {
+        static void Main (string[] args)
+        {
+            int[] array = new int[10];
+            int[] array2 = new int[10];
+            Random rnd = new Random();
+            int[] array3 = new int[10];
+            for (int i = 0; i<array.Length; i++)
+            {
+                array[i] = rnd.Next(10, 100);
+                
+            }
+            for (int i = 0; i < array2.Length; i++)
+            {
+                array2[i] = rnd.Next(10, 100);
+
+            }
+            for (int i = 0; i < array.Length; i++)
+            {
+                array3[i] = array[i] * array2[i];
+                Console.WriteLine($" array3[i]");
+            }
+            
+        }
+
+    }
+}
