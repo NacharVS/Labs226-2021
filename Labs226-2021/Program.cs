bbc0d78b1689c58eb49c452322f5bda478e56651
r3dinc <r3dinc@yandex.ru>
2021-09-23 12:59:16 +0300

    home work number one

--- Labs226-2021/Program.cs
+++ Labs226-2021/Program.cs
@@ -5,7 +5,10 @@ namespace Labs226_2021
     class Program
     {
         static void Main(string[] args)
-        {           
+        {
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
         }
     }
 }
