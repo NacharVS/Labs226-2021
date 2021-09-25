eaa625f9355013e24d8598883cd0848d2169cb37
r3dinc <r3dinc@yandex.ru>
2021-09-25 14:18:34 +0300

    examples and tasks

--- Labs226-2021/ArrayOperations.cs
+++ Labs226-2021/ArrayOperations.cs
@@ -8,6 +8,24 @@ namespace Labs226_2021
 {
     class ArrayOperations
     {
+        // 1. Найти максимальный элемент матрицы в каждой строке
+        // 2. Найти сумму элементов главной диагонали матрицы
+
+
+        public static void TwoDimensionalArrayEx1()
+        {
+            int[,] array = new int[10, 5];
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    array[i, j] = new Random().Next(10, 50);
+                    Console.Write($"{array[i, j]} ");
+                }
+                Console.WriteLine();
+            }
+        }
+
         public static void ArrayBubbleSort()
         {
             int[] array = new int[10];
--- Labs226-2021/Program.cs
+++ Labs226-2021/Program.cs
@@ -7,7 +7,16 @@ namespace Labs226_2021
     {
         static void Main(string[] args)
         {
-           
+            int[,] array = new int[10, 10];
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    array[i, j] = new Random().Next(10, 50);
+                    Console.Write($"{array[i,j]} ");
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
