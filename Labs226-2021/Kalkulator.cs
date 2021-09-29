0244bce50816cfe9b944a6f9aea2c2197ef7dc6a
Ichisa6 <sublosd69@gmail.com>
2021-09-29 23:28:02 +0300

    Samdan

---
+++ Labs226-2021/Kalkulator.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Задача_2
+{
+    class Kalkulator
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Выберете действие(+, -, /, *)");
+            char deistvie = char.Parse(Console.ReadLine());
+            Console.WriteLine("Введите первое значение");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите второе значение");
+            int b = int.Parse(Console.ReadLine());
+
+            if (deistvie == '+')
+            {
+                Console.WriteLine("Ответ:");
+                Console.WriteLine(a + b);
+            }
+            else if (deistvie == '-')
+            {
+                Console.WriteLine("Ответ:");
+                Console.WriteLine(a - b);
+            }
+            else if (deistvie == '/')
+            {
+                if (a > b)
+                {
+                    Console.WriteLine("Ответ:");
+                    Console.WriteLine(a / b);
+                }
+                else
+                {
+                    Console.WriteLine("Ответ:");
+                    Console.WriteLine(b / a);
+                }
+            }
+            else if (deistvie == '*')
+            {
+                Console.WriteLine("Ответ:");
+                Console.WriteLine(a * b);
+            }
+            else
+            {
+                Console.WriteLine("Введенное действие неизвестно");
+            }
+            int g = int.Parse(Console.ReadLine());
+        }
+    }
+}
