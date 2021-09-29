e20166c0b8862bba4e1c0314602ee3e31bef60a5
Shizukata <90610585+Shizukata@users.noreply.github.com>
2021-09-29 16:26:03 +0300

    Putin

---
+++ Labs226-2021/Calculator.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Задача_2
+{
+    class Calculator
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Калькулятор.");
+            Console.WriteLine("Выберете действие(+, -, /, *)");
+            char o = char.Parse(Console.ReadLine());
+            Console.WriteLine("Введите первое значение");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите второе значение");
+            int b = int.Parse(Console.ReadLine());
+
+            if (o == '+')
+            {
+                Console.WriteLine("Ответ:");
+                Console.WriteLine(a + b);
+            }
+            else if (o == '-')
+            {
+                Console.WriteLine("Ответ:");
+                Console.WriteLine(a - b);
+            }
+            else if (o == '/')
+            {
+                if (a > b)
+                {
+                    Console.WriteLine("Ответ:");
+                    Console.WriteLine(a / b);
+                }
+                else
+                {
+                    Console.WriteLine("Ответ:");
+                    Console.WriteLine(b / a);
+                }
+            }
+            else if (o == '*')
+            {
+                Console.WriteLine("Ответ:");
+                Console.WriteLine(a * b);
+            }
+            else
+            {
+                Console.WriteLine("Введенное действие неизвестно");
+            }
+            int g = int.Parse(Console.ReadLine());
+        }
+    }
+}
