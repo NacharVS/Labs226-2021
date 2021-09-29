807395a9bdf5027d80e4918ea243997b70034da0
grosfae <90610580+grosfae@users.noreply.github.com>
2021-09-29 18:11:41 +0300

    Work

---
+++ Labs226-2021/Lock.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tests
+{
+    class Kod
+    {
+        public static void Lock()
+        { 
+            int sum = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < 10; i++)
+            {
+                for (int d = 0; d < 10; d++)
+                {
+                    for (int k = 0; k < 10; k++)
+                    {
+                        if (i + d + k == sum)
+                        {
+                            if (i != d)
+                            {
+                                if (d != k)
+                                {
+                                    Console.WriteLine($"{ i}{ d}{ k}");
+                                }
+                            }
+
+                        }
+
+                    }
+                }
+            }
+        }
+    }
+}
