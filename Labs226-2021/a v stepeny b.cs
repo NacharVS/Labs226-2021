9f778a7f0a8d24f49e29a58937b8b03b17bdd6a5
Andrey36568 <53220738+Andrey36568@users.noreply.github.com>
2021-09-29 23:00:18 +0300

    Sasuke

---
+++ Labs226-2021/a v stepeny b.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Labs226_2021
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите степень");
+            int b = int.Parse(Console.ReadLine());
+            int k = a;
+            int i = 0;
+            while (i != b)
+            {
+                a = a * k;
+                i = i + 1;
+            }
+            Console.WriteLine($"Число {k} в {b} равняется {a}");
+        }
+    }
+}
