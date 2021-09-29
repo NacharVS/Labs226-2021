95248fa71df4cd9695f51c7643855b5b305291dd
Ichisa6 <sublosd69@gmail.com>
2021-09-29 22:40:08 +0300

    Create a_v_stepeni_b.cs

---
+++ Labs226-2021/a_v_stepeni_b.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Vozvedenie_v_stepenb
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите степень");
+            int b = int.Parse(Console.ReadLine());
+            int n = a;
+            int i = 0;
+            while (i != b)
+            {
+                a = a * n;
+                i = i + 1;
+            }
+            Console.WriteLine($"Число {n} в {b} равняется {a}");
+        }
+    }
+}
