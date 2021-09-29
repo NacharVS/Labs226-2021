bc81bab1c9dcf8b40f6f563652ed410693c5431e
Andrey36568 <53220738+Andrey36568@users.noreply.github.com>
2021-09-29 23:22:25 +0300

    Sakura

---
+++ Labs226-2021/Naxodim Ploshad Shara.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading;
+
+namespace Labs226_2021
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Нахождение площади шара по R");
+            Console.WriteLine("Введите значение R");
+            Console.WriteLine("R = ");
+
+            int R = int.Parse(Console.ReadLine());
+            double p = Math.PI;
+            R = R * R;
+
+            Console.WriteLine($"S шара = {4 * p * R}");
+
+            int a = int.Parse(Console.ReadLine());
+        }
+    }
+}
