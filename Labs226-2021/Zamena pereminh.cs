cdfd67ba35770ce16d20cb4a54ff7d088dae588b
Andrey36568 <53220738+Andrey36568@users.noreply.github.com>
2021-09-29 23:29:56 +0300

    Madara

---
+++ Labs226-2021/Zamena pereminh.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading;
+
+namespace Labs226_2021
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Замена переменных");
+
+            Console.WriteLine("Введите значение a");
+            Console.WriteLine("a =");
+            int a = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите значение b");
+            Console.WriteLine("b =");
+            int b = int.Parse(Console.ReadLine());
+
+            a = a + b;
+            b = a - b;
+            a = a - b;
+
+            Console.WriteLine($"a ={a}; b ={b}");
+
+        }
+    }
+
+}
