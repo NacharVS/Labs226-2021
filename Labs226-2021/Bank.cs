b5f6c51c16155b095ade348ca20db959220da975
Shizukata <90610585+Shizukata@users.noreply.github.com>
2021-09-29 16:35:58 +0300

    fet

---
+++ Labs226-2021/Bank.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Задача_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Добро пожаловать в наш банк, прошу ознакомиться с процентными ставками по вкладу:");
+            Console.WriteLine("< 100р = 0%");
+            Console.WriteLine("100 - 1000р = 5%");
+            Console.WriteLine("1000 - 5000р = 7%");
+            Console.WriteLine("5000 - 50000р = 9%");
+            Console.WriteLine("> 50000 = 12%");
+            Console.WriteLine("Введите сумму вклада");
+            Console.WriteLine("Вклад = ");
+            int d = int.Parse(Console.ReadLine());
+            if(d < 100)
+            {
+                Console.WriteLine("Через год вы получите данную сумму = ");
+                Console.WriteLine(d);
+            }
+            else if (d >= 100 && d <=1000)
+            {
+                Console.WriteLine("Через год вы получите данную сумму = ");
+                Console.WriteLine(d + (d * 0.05));
+            }
+            else if (d > 1000 && d <= 5000)
+            {
+                Console.WriteLine("Через год вы получите данную сумму = ");
+                Console.WriteLine(d + (d * 0.07));
+            }
+            else if (d > 5000 && d <= 50000)
+            {
+                Console.WriteLine("Через год вы получите данную сумму = ");
+                Console.WriteLine(d + (d * 0.09));
+            }
+            else if (d > 50000)
+            {
+                Console.WriteLine("Через год вы получите данную сумму = ");
+                Console.WriteLine(d + (d * 0.12));
+            }
+            int g = int.Parse(Console.ReadLine());
+        }
+    }
+}
---
+++ Labs226-2021/Zamena_Perem2.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+    class Program0
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Замена переменных");
+
+            Console.WriteLine("Введите значение a");
+            Console.WriteLine("a =");
+            int a = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите значение b");
+            Console.WriteLine("b =");
+            int b = int.Parse(Console.ReadLine());
+
+            a = a + b;
+            b = a - b;
+            a = a - b;
+
+            Console.WriteLine($"a ={a}; b ={b}");
+
+        }
+    }
+    class Program1
+    {
+        static void Main1(string[] args)
+        {
+            Console.WriteLine("Введите значение b");
+            int c = int.Parse(Console.ReadLine());
+        }
+    }
+}
+
+
