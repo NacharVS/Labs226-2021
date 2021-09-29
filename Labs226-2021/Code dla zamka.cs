2fae10882e5ca6174ac5e26cd7b6265b994d018c
Andrey36568 <53220738+Andrey36568@users.noreply.github.com>
2021-09-29 22:39:43 +0300

    Naruto

---
+++ Labs226-2021/Code dla zamka.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading;
+
+namespace Labs226_2021
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введи значение суммы для которой мы будем подбирать комбинацию цифр:");
+            int a = int.Parse(Console.ReadLine());
+            for (int n = 0; n < 10; n++)
+                for (int y = n + 1; y < 10; y++)
+                    for (int x = y + 1; x < 10; x++)
+                        if (n + y + x == a && n != y && n != x && x != y)
+                            Console.WriteLine($"Вот одна из вариаций, а ну-ка попробуй её:{n}{y}{x}");
+        }
+    }
+}
