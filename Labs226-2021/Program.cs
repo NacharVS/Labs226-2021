c0501cc7215689f656c1582ef29585225eeb77b9
Biburat123 <91465180+Biburat123@users.noreply.github.com>
2021-09-30 12:42:25 +0300

    Work

---
+++ Labs226-2021/Program.cs
@@ -0,0 +1,15 @@
+﻿using _1;
+using System;
+
+namespace _9878
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int [,] array = new int [7,7];
+            
+            MatrixArrays.MaxElementVStroke(array);
+        }
+    }
+}
