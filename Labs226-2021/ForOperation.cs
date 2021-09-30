accf3b6426f734083e4f5b7c9ac72a4f0f8d2916
DinarShigapov <dinarhertz@gmail.com>
2021-09-30 20:41:38 +0300

    ForOperation
    Class work

---
+++ Labs226-2021/ForOperation.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Labs226_2021
+{
+    class ForOperation
+    {
+        public static void Degree()
+        {
+            Console.WriteLine("Напишите число: ");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Напишите степень: ");
+            int b = int.Parse(Console.ReadLine());
+            int res = 1;
+            for (int i = 0; i < b; i++)
+            {
+                res *= a;
+
+            }
+            Console.WriteLine();
+            Console.WriteLine($"Число {a} в степени {b}: ");
+            Console.WriteLine(res);
+
+
+        }
+        public static void Reverse()
+        {
+            Console.WriteLine("1234 - 4321");
+            Console.WriteLine("Введите число: ");
+            int a = int.Parse(Console.ReadLine());
+            int res = 0;
+
+            while (a > 0)
+            {
+                res *= 10;
+                res += a % 10;
+                a /= 10;
+            }
+            Console.WriteLine(res);
+
+        }
+    }
+}
