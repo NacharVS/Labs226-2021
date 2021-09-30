e3f7e30907ffae18ec7b4762db402bdbd57ac762
Biburat123 <91465180+Biburat123@users.noreply.github.com>
2021-09-30 12:54:13 +0300

    Work

---
+++ Labs/SameArrays.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _1
+{
+    class SameArrays
+    {
+        public static void SameArr(int[] array, int[] array2)
+        {
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = new Random().Next(1, 10);
+                Console.Write($" {array[i]}");
+            }
+
+            Console.WriteLine();
+
+            for (int i = 0; i < array2.Length; i++)
+            {
+                array2[i] = new Random().Next(1, 10);
+                Console.Write($" {array2[i]}");
+            }
+
+            Console.WriteLine();
+            Console.WriteLine();
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int c = i + 1; c < array.Length; c++)
+                {
+                    if (array[i] == array[c])
+                    {
+                        array[c] = 0;
+                    }
+                }
+
+                for (int g = 0; g < array2.Length; g++)
+                {
+                    if (array[i] == array2[g])
+                    {
+
+                        Console.Write(" " + $"{array2[g]}");
+                        array[i] = 0;
+
+                    }
+
+
+                }
+            }
+        }
+    }
+}
