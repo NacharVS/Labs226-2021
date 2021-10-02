d4e8d68f56a94a448417d0ad1da3317d1cb7fc55
Shizukata <90610585+Shizukata@users.noreply.github.com>
2021-10-02 14:00:07 +0300

    Это наш последний заезд

---
+++ Labs226-2021/Задания/Array First.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _1
+{
+    class ArraysFirst
+    {
+        public static void MaxMin(int[] array)
+        {
+
+            int min = 10; int max = array[0];
+
+            /* Создание, заполнение и нахождение мин макс значение массива */
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = new Random().Next(0, 10);
+                Console.Write($" {array[i]}");
+                if (array[i] > max)
+                    max = array[i];
+                if (array[i] < min)
+                    min = array[i];
+            }
+            Console.WriteLine();
+            Console.WriteLine($"max = {max} min = {min}");
+        }
+
+        public static void Umno(int[] array, int[] array2)
+        {
+
+            int[] array3 = new int[10];
+
+            /* Создание 2х строк массива и их заполнение */
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = new Random().Next(0, 2);
+                array2[i] = new Random().Next(0, 2);
+                Console.Write($" {array[i]}");
+            }
+
+            Console.WriteLine();
+
+            /* Сложение строк массива поячеечно */
+            for (int i = 0; i < array2.Length; i++)
+            {
+                Console.Write($" {array2[i]}");
+                array3[i] = array[i] + array2[i];
+            }
+            Console.WriteLine();
+
+            for (int i = 0; i < array3.Length; i++)
+            {
+                Console.Write($" {array3[i]}");
+            }
+        }
+    }
+}
---
+++ Labs226-2021/Задания/a^b.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace a_b
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите число:");
+            int a = int.Parse(Console.ReadLine());
+
+            Console.Write("Введите степень:");
+            int b = int.Parse(Console.ReadLine());
+
+            int ctepen = 1;
+            for (int i = 0; i < b; i++)
+            {
+                ctepen *= a;
+
+            }
+            Console.WriteLine();
+
+            Console.Write($"Ответ = ");
+            Console.Write(ctepen);
+        }
+    }
+}
