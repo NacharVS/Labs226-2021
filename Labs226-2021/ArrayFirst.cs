4fd51a63b3fb46e924325a96ce6107988a56ba3e
Ramazan226 <90611056+Ramazan226@users.noreply.github.com>
2021-10-02 13:27:00 +0300

    Update ArrayFirst.cs

--- Labs226-2021/ArrayFirst.cs
+++ Labs226-2021/ArrayFirst.cs
@@ -51,32 +51,4 @@ namespace D-7
             }
         }
 
-        public static void Same(int[] array)
-        {
-
-            for (int i = 0; i < array.Length; i++)
-            {
-                array[i] = new Random().Next(1, 10);
-                Console.Write($" {array[i]}");
-            }
-
-            for (int i = 0; i < array.Length; i++)
-            {
-                for (int j = i + 1; j < array.Length; j++)
-                {
-                    if (array[i] == array[j])
-                    {
-                        array[i] = 0;
-                        array[j] = 0;
-                    }
-                }
-            }
-            Console.WriteLine();
-
-            for (int i = 0; i < array.Length; i++)
-            {
-                Console.Write($" {array[i]}");
-            }
-        }
-    }
-}
+    
