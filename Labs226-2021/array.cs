67d71e8dee996e5a9cdf5bbbe9c8f978ce58869c
kamalooffild <dallalal322@gmail.com>
2021-10-02 13:49:16 +0300

    matrix
    summa  diagonaley & max element

---
+++ Labs226-2021/array.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Labs226_2021
+{
+    class array
+    {
+        public static void summadiagonaley()
+        {
+            int a = 0;
+            int[,] array = new int[10, 10];
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    array[i, j] = new Random().Next(10, 50);
+                    Console.Write($"{array[i, j]} ");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1);j++)
+                {
+                    if (i == j)
+                    {
+                        a = a + array[i, j];
+                    }
+                }
+            }
+            Console.WriteLine(a);
+        }
+        public static void maxelement()
+        {
+            int[] array = new int[10];
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = new Random().Next(10, 100);
+                Console.Write($"{array[i]} ");
+            }
+            int max = array[0];
+            for (int i = 1; i < array.Length; i++)
+            {
+                if (array[i] > max) max = array[i];
+            }
+            Console.WriteLine();
+            Console.WriteLine($"Максимальное значение массива равно {max}");
+        }
+    }
+}
