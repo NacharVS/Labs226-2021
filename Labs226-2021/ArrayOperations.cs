4c683c4a724433269f504242513ddd0d04fcce63
r3dinc <r3dinc@yandex.ru>
2021-10-06 16:32:39 +0300

    methods

--- Labs226-2021/ArrayOperations.cs
+++ Labs226-2021/ArrayOperations.cs
@@ -8,21 +8,43 @@ namespace Labs226_2021
 {
     class ArrayOperations
     {
-        // 1. Найти максимальный элемент матрицы в каждой строке
-        // 2. Найти сумму элементов главной диагонали матрицы
-
-
-        public static void TwoDimensionalArrayEx1()
-        {
-            int[,] array = new int[10, 5];
+        public static void ArrayGeneration(int[,] array)
+        {        
             for (int i = 0; i < array.GetLength(0); i++)
             {
                 for (int j = 0; j < array.GetLength(1); j++)
                 {
-                    array[i, j] = new Random().Next(10, 50);
+                    array[i, j] = new Random().Next(2);
                     Console.Write($"{array[i, j]} ");
                 }
                 Console.WriteLine();
+            }
+        }
+
+        public static void ArrayGeneration(int[] array)
+        {
+                for (int i = 0; i < array.Length; i++)
+                {
+                    array[i] = new Random().Next(2);
+                    Console.Write($"{array[i]} ");
+                }
+        }
+
+        public static void ArrayGeneration(int[] array, int n)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = new Random().Next(n);
+                Console.Write($"{array[i]} ");
+            }
+        }
+
+        public static void ArrayGeneration(int[] array, int n, int m)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = new Random().Next(n,m);
+                Console.Write($"{array[i]} ");
             }
         }
 
