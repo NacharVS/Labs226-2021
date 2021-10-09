684f5491ab8424ec6e6aaf88215b5b381d5cd927
Timghf <tsharafutdinov300@gmail.com>
2021-10-09 13:59:04 +0300

    Bakaleya

--- Labs226-2021/Bakaleya.cs
+++ Labs226-2021/Bakaleya.cs
@@ -8,9 +8,17 @@ namespace Labs226_2021
         public static void Pokypka()
         {
             string[] array = new string[10] { "Хлеб", "Батон", "Молоко", "Виноград", "Яблоки", "Бананы", "Картошка", "Вишня", "Масло", "Растительное масло" };
+            double[] basket = new double[10];
+            double[] price = new double[10];
 
+            string[] kg = new string[10] { "-", "-", "-", "Виноград", "Яблоки", "Бананы", "Картошка", "Вишня", "-", "-" };
 
-            int[] array2 = new int[10] {34, 35, 60, 500, 80, 80, 40, 200, 120, 100};
+            string stop = "Нет";
+            string ans = "Да";
+            double res = 0;
+            int tovar = 0;
+
+            int[] array2 = new int[10] { 34, 35, 60, 500, 80, 80, 40, 200, 120, 100 };
             for (int i = 0; i < array.Length; i++)
             {
                 if (i == 0 || i == 1 || i == 2 || i == 8 || i == 9)
@@ -25,25 +33,65 @@ namespace Labs226_2021
 
             }
 
-            string[] basket = new string[] { };
-            Console.WriteLine("Выбирите позицию товара: ");
-            int choice = int.Parse(Console.ReadLine());
-            
-            if (choice == 1)
+            Console.WriteLine();
+
+
+            while (stop != ans)
             {
-                Console.WriteLine(array[0]);
-                Console.WriteLine("Выберите количество товара");
-                choice = int.Parse(Console.ReadLine());
-                Console.WriteLine($"{array[0]} =  {array2[0] * choice}");
+                Console.WriteLine("Выберите позицию товара: ");
+                tovar = int.Parse(Console.ReadLine());
+                tovar -= 1;
+
+                Console.WriteLine("Выберите шт/граммах товара: ");
+                basket[tovar] = double.Parse(Console.ReadLine());
+
+                Console.WriteLine();
+                if (array[tovar] == kg[tovar])
+                {
+                    Console.WriteLine($"{array[tovar] } = {array2[tovar] * (basket[tovar]) / 1000}");
+
+                }
+                else
+                {
+                    Console.WriteLine($"{array[tovar] } = {array2[tovar] * basket[tovar]}");
+                }
+
+                Console.WriteLine();
+                Console.WriteLine("Хотите ли вы продолжить покупку?\nНапишите 'Да' или 'Нет' ");
+                ans = Console.ReadLine();
+                Console.WriteLine();
+
+
+            }
+            Console.WriteLine("_______________");
+            Console.WriteLine("Моя корзина");
+            Console.WriteLine();
+            for (int i = 0; i < basket.Length - 1; i++)
+            {
+                if (basket[i] != 0)
+                {
+                    if (array[i] == kg[i])
+                    {
+                        price[i] = array2[i] * (basket[i] / 1000);
+                    }
+                    else
+                    {
+                        price[i] = array2[i] * basket[i];
+                    }
+
+                    Console.WriteLine($"{array[i]} = {price[i]}");
+
+                }
 
             }
 
+            for (int i = 0; i < price.Length -1; i++)
+            {
+                res = res + price[i];
+            }
+            Console.Write("Итого: ");
+            Console.WriteLine(res);
 
-
-
-
-                
         }
-
     }
 }
