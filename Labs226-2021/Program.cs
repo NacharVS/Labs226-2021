1f08cd6e1bef46a7db51598e4f10b56944cb5dfe
Timghf <dinarhertz@gmail.com>
2021-10-23 14:30:17 +0300

    Update Program.cs

--- Labs226-2021/Program.cs
+++ Labs226-2021/Program.cs
@@ -8,22 +8,7 @@ namespace Labs226_2021
     {
         static void Main(string[] args)
         {
-            Peasant peasant1 = new Peasant("peasant1", 1, 6, 3, 30, 3, "Bridge");
-            peasant1.Attack();
-            peasant1.Produce();
-            peasant1.CancelAction();
-            peasant1.Moving();
-            peasant1.HoldPositon();
-            peasant1.Death();
-            Console.WriteLine();
-
-            Healer healer1 = new Healer("healer1", 1, 6, 3, 50, 3, "hospital");
-            healer1.Attack();
-            healer1.Produce();
-            healer1.CancelAction();
-            healer1.Moving();
-            healer1.HoldPositon();
-            healer1.Death();
+            Activate.Activ();
         }
     }
 }
