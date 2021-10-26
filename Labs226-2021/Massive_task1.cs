282b602dcdfaf741a284c6ba7cdd525ce17731a0
Vorand69 <90610674+Vorand69@users.noreply.github.com>
2021-10-26 11:34:17 +0300

    Create Massive_task1.cs
    1

---
+++ Labs226-2021/Massive_task1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace task_1_MinMax
+{
+    class MaxMinMassive
+    {
+        public static void MM()
+        {
+            int[] nums = new int[10];
+            int min = 100; int max = nums[0];
+            for (int i = 0; i < nums.Length; i++)
+            {
+                nums[i] = new Random().Next(0, 100);
+                Console.Write($" {nums[i]}");
+                if (nums[i] > max)
+                    max = nums[i];
+                if (nums[i] < min)
+                    min = nums[i];
+            }
+            Console.WriteLine($"max = {max} min = {min}");
+        }
+
+    }
+}
+
