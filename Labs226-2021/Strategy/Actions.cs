068e348156e39a6d27d4db4d4b32079a8992fb66
r3dinc <r3dinc@yandex.ru>
2021-10-28 12:29:44 +0300

    start

---
+++ Labs226-2021/Strategy/Actions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Labs226_2021.Strategy
+{
+    class Actions
+    {
+        public static void Battle(Unit unit1, Unit unit2)
+        {
+           
+        }
+    }
+}
--- Labs226-2021/Strategy/Unit.cs
+++ Labs226-2021/Strategy/Unit.cs
@@ -6,19 +6,43 @@ namespace Labs226_2021.Strategy
 {
     class Unit
     {
-        public string typeUnit;
-        public int currentHealth;
-        public int maxHealth;
-        public int mindamage;
-        public int maxdamage;
-        public int speed;
-        public int attackSpeed;
+
+        // public
+        // private 
+        // protected
+        // internal
+        private string _typeUnit;
+        private int _currentHealth;
+        private readonly int _maxHealth;
+        private int _mindamage;
+        private int _maxdamage;
+        private int _speed;
+        private int _attackSpeed;
+
+        public int CurrentHealth
+        {
+            get => _currentHealth;
+            set
+            {
+                if (value > _maxHealth)
+                {
+                    _currentHealth = _maxHealth;
+                }
+                else
+                {
+                    _currentHealth = value;
+                }
+            }
+        }
+
+        public int Mindamage { get => _mindamage; set => _mindamage = value; }
+        public int Maxdamage { get => _maxdamage; set => _maxdamage = value; }
 
         public void Attack()
         {
-            int hit = new Random().Next(mindamage, maxdamage);
-            Console.WriteLine($"{typeUnit} delivered {hit} mlee damage");
+            
         }
+
 
         public void Death()
         {
