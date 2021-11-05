614d91a36b8439cdfaed6dc231116660527bc783
NikitaMatv <kuzmenko_ka@mail.ru>
2021-11-05 10:30:43 +0300

    цц

---
+++ Labs226-2021/Product.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Labs226_2021
+{
+    class Product
+    {
+        public string name;
+        public double prise;
+        public int qouter;
+
+        public Product(string name, double prise, int qouter)
+        {
+            this.name = name;
+            this.prise = prise;
+            this.qouter = qouter;
+        }
+    }
+}
