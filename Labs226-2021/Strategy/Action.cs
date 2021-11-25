51ac99db613d9a528e2ba0a6c20bfe62f5bf59a4
Timghf <dinarhertz@gmail.com>
2021-11-25 13:16:48 +0300

    Update Action.cs

--- Labs226-2021/Strategy/Action.cs
+++ Labs226-2021/Strategy/Action.cs
@@ -1,17 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using System.Threading;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Text;
+//using System.Threading;
 
-namespace Labs226_2021.Strategy
-{
-    class Action: Unit
-    {
-        public static void Battle(Unit unit1, Unit unit2)
-        {
-            Thread.Sleep(1000);
-            Console.WriteLine($"unit {} hited unit {}");
-            unit2.Attack(unit1.Inflictdamage());
-        }
-    }
-}
+//namespace Labs226_2021.Strategy
+//{
+//    class Action: Unit
+//    {
+//        public static void Battle(Unit unit1, Unit unit2)
+//        {
+//            Thread.Sleep(1000);
+//            Console.WriteLine($"unit {} hited unit {}");
+//            unit2./*Attack*/(unit1.Inflictdamage());
+////        }
+//    }
+//}
