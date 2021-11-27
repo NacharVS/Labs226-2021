9d9fc9128c7b6fd0b4942beddf7f1e8c09dc566f
Biburat123 <90610603+NikitaMatv@users.noreply.github.com>
2021-11-27 13:45:52 +0300

    W

--- Labs226-2021/Program.cs
+++ Labs226-2021/Program.cs
@@ -1,32 +1,32 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using Labs226_2021.Strategy;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Text;
+//using Labs226_2021.Strategy;
 
-namespace Labs226_2021
-{
-    class Task
-    {
-        static void Main ()
-        {
+//namespace Labs226_2021
+//{
+//    class Task
+//    {
+//        static void Main ()
+//        {
 
-            Peasant Warrior1 = new Peasant();
-            Mage Warrior2 = new Mage();
-            Warrior2.Name = "Alalp"; Warrior1.Name = "Lokit";
+//            Peasant Warrior1 = new Peasant();
+//            Mage Warrior2 = new Mage();
+//            Warrior2.Name = "Alalp"; Warrior1.Name = "Lokit";
 
-            Warrior2.MinDamage = 5; Warrior1.MinDamage = 5; Warrior2.Health = 50;
-            Warrior2.MaxDamage = 7; Warrior1.MaxDamage = 9; Warrior1.Health = 50;
+//            Warrior2.MinDamage = 5; Warrior1.MinDamage = 5; Warrior2.Health = 50;
+//            Warrior2.MaxDamage = 7; Warrior1.MaxDamage = 9; Warrior1.Health = 50;
            
 
-            while (Warrior2.Health != 0 && Warrior1.Health != 0)
-            {
-                Actions.WarriorVSArcher(Warrior1, Warrior2);
-            }
+//            while (Warrior2.Health != 0 && Warrior1.Health != 0)
+//            {
+//                Actions.WarriorVSArcher(Warrior1, Warrior2);
+//            }
 
 
-        }
+//        }
        
 
-    }
-    }
+//    }
+//    }
 
