f4111044a5b954c8945c47c5023919dcd2df3516
Musisin228 <90610962+Musisin228@users.noreply.github.com>
2021-11-27 14:05:08 +0300

    Ноябрь

--- Labs226-2021/Program.cs
+++ Labs226-2021/Program.cs
@@ -7,8 +7,8 @@ namespace Labs226_2021
     {
         static void Main(string[] args)
         {
-            BankAccount account1 = new BankAccount("Ivan", 5000);
-            BankAccount account2 = new BankAccount("Kirill", 3000);
+            BankAccount account1 = new BankAccount("Ivan", 5000, 1971);
+            BankAccount account2 = new BankAccount("Kirill", 3000, 1984);
 
             account1.ShowInfo();
             account2.ShowInfo();
@@ -17,15 +17,17 @@ namespace Labs226_2021
             BankAccount.GetProfit(account2, 10);
             account1.ShowInfo();
             account2.ShowInfo();
-            BankAccount.Withdraw(account1, 20000);
-
+            BankAccount.Withdraw(account1, 200);
+            BankAccount.Transaction(account1, account2);
+            account1.ShowInfo();
+            account2.ShowInfo();
         }
     }
     class BankAccount
     {
         public string name;
         public double balance;
-
+        private int _yearofBirth;
         public static double rate = 0.007;
         public static double minWidtraw = 100;
         public static double maxWidtraw = 5000;
@@ -34,35 +36,75 @@ namespace Labs226_2021
 
 
 
-        public BankAccount(string name, double balance)
+        public BankAccount(string name, double balance, int yezarofBirth)
         {
             this.name = name;
             this.balance = balance;
+            this.Age = yezarofBirth;
         }
+        public int Age
+        {
+            get
+            {
+                return _yearofBirth;
+            }
 
+            set
+            {
+                if (value > 1900 & value <= DateTime.Now.Year & DateTime.Now.Year - value >= 18)
+                    _yearofBirth = DateTime.Now.Year - value;
+                else
+                {
+                    Console.WriteLine("Введён неверный год" + " " + $" { name}");
+                }
+            }
+        }
         public void ShowInfo()
         {
-            System.Console.WriteLine($"{name} - {balance}");
+            if (Age > 18)
+                System.Console.WriteLine($"{name} - {balance} - {Age}");
+            else
+            {
+                Console.WriteLine("Ваш ваозраст слишком мал");
+            }
         }
 
         public static void GetProfit(BankAccount acc, int month)
         {
-            for (int i = 0; i < month; i++)
             {
-                acc.balance += acc.balance * rate;
+
+                if (acc.Age > 0)
+                {
+                    for (int i = 0; i < month; i++)
+                    {
+                        acc.balance += acc.balance * rate;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Ваш ваозраст слишком мал");
+                }
+
             }
         }
 
         public void Deposit(BankAccount acc, double money)
         {
-            if (money < minDeposit)
+            if (Age > 0)
             {
-                Console.WriteLine("Слишком маленькая сумма, минимальный депозит составляет 10");
+                if (money < minDeposit)
+                {
+                    Console.WriteLine("Слишком маленькая сумма, минимальный депозит составляет 10");
+                }
+                else
+                {
+                    acc.balance = acc.balance + money;
+                    Console.WriteLine($"На баланс {acc.name} было зачисленно {money}, теперь его баланс составляет {acc.balance}");
+                }
             }
             else
             {
-                acc.balance = acc.balance + money;
-                Console.WriteLine($"На баланс {acc.name} было зачисленно {money}, теперь его баланс составляет {acc.balance}");
+                Console.WriteLine("Ваш ваозраст слишком мал");
             }
 
         }
@@ -81,12 +123,45 @@ namespace Labs226_2021
             {
                 acc.balance = acc.balance - money;
                 Console.WriteLine($"{acc.name} снял {money} и на его счету осталось {acc.balance}");
-            }
+            } 
+
         }
 
         public static void Transaction(BankAccount accSeller, BankAccount accGetter)
         {
+            if (accSeller.Age > 0 & accGetter.Age > 0)
+            {
+                Console.WriteLine($"Введите сумму перевода");
+                double p = double.Parse(Console.ReadLine());
+                if (p < 0)
+                {
+                    Console.WriteLine("Недопустимое зачение");
+                }
+                else
+                {
+                    double k = (p * comissionForTransaction) + p;
+                    if (k > accSeller.balance)
+                    {
+                        Console.WriteLine("На вашем счете недостаточно средств");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{accSeller.name}, Вы хотите совершить перевод {accGetter.name}?");
+                        string o = Console.ReadLine();
+                        if (o == "да")
+                        {
 
+                            accGetter.balance = accGetter.balance + p;
+                            Console.WriteLine($"С учётом коммисии с вашего счета вычлось {k}");
+                            accSeller.balance = accSeller.balance - k;
+                        }
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("Ваш ваозраст слишком мал");
+            }
         }
     }
 
