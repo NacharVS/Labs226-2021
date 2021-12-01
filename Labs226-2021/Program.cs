4b4b96cb43ba8881ee13cbea451532b5e40626d5
ferriari <91202201+ferriari@users.noreply.github.com>
2021-12-01 12:29:47 +0300

    Update Program.cs

--- Labs226-2021/Program.cs
+++ Labs226-2021/Program.cs
@@ -1,12 +1,36 @@
-﻿using System;
+﻿using Labs226_2021.Strategy;
+using System;
+using System.Threading;
 
-namespace ConsoleApp1
+namespace Labs226_2021
 {
     class Program
     {
         static void Main(string[] args)
         {
-            ConsoleApp3.Backaley.BackaleySpisok();
+            BankAccount account1 = new BankAccount("Ivan", 5000, 1890);
+            BankAccount account2 = new BankAccount("Kirill", 3000, 1999);
+            BankAccount account3 = new BankAccount("Elena", 6000, 2000);
+
+            account1.ShowInfo();
+            account2.ShowInfo();
+            account3.ShowInfo();
+            BankAccount.rate = 0.004;
+            BankAccount.GetProfit(account1, 6);
+            BankAccount.GetProfit(account2, 6);
+            BankAccount.GetProfit(account3, 6);
+            account1.ShowInfo();
+            account2.ShowInfo();
+            account3.ShowInfo();
+            account1.Deposit(9);
+
+            account1.ShowInfo();
+            account1.Withdraw(5001);
+            account1.ShowInfo();
+            BankAccount.Transaction(account1, account2, 5000);
+            account1.ShowInfo();
+            account2.ShowInfo();
+
         }
     }
 }
