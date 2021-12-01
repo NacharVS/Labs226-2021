07810362c74d055c2edffbb21d8c3c4b6e0f2dd8
ferriari <91202201+ferriari@users.noreply.github.com>
2021-12-01 14:05:43 +0300

    work

--- Labs226-2021/Account.cs
+++ Labs226-2021/Account.cs
@@ -8,13 +8,13 @@ namespace Labs226_2021
     {
         static public void Sber()
         {
-            AccountBank account1 = new SberBank("Ivan", 15000);
-            AccountBank account2 = new SberBank("Kirill", 13000);
-            AccountBank account3 = new SberBank("Elena", 16000);
+            SberBank account1 = new SberBank("Ivan", 15000);
+            SberBank account2 = new SberBank("Kirill", 13000);
+            SberBank account3 = new SberBank("Elena", 16000);
 
 
-            account1.YearOfBirth = 2003;
-            account2.YearOfBirth = 2002;
+            account1.YearOfBirth = 2005;
+            account2.YearOfBirth = 2004;
             account3.YearOfBirth = 2002;
 
 
@@ -23,7 +23,7 @@ namespace Labs226_2021
             account2.ShowInfo();
             account3.ShowInfo();
             Console.WriteLine();
-
+           
             SberBank.GetProfit(account1, 6);
             SberBank.GetProfit(account2, 6);
             SberBank.GetProfit(account3, 6);
@@ -34,24 +34,24 @@ namespace Labs226_2021
             Console.WriteLine();
 
             account1.ShowInfo();
-            AccountBank.Deposit(9, 6, account1);
+            SberBank.Deposit(9, 6, account1);
             account2.ShowInfo();
-            AccountBank.Deposit(2000, 6, account2);
+            SberBank.Deposit(2000, 6, account2);
             account3.ShowInfo();
-            AccountBank.Deposit(2000, 6, account3);
+            SberBank.Deposit(2000, 6, account3);
             Console.WriteLine();
 
             account1.ShowInfo();
-            AccountBank.Withdraw(account1, 99);
+            SberBank.Withdraw(account1, 99);
             account2.ShowInfo();
-            AccountBank.Withdraw(account2, 10000);
+            SberBank.Withdraw(account2, 10000);
             account3.ShowInfo();
-            AccountBank.Withdraw(account3, 1000);
-
-
+            SberBank.Withdraw(account3, 1000);
+    
+            
             Console.WriteLine();
 
-            Account.Transaction(account1, account2, 5000);
+            SberBank.Transaction(account1, account2, 5000);
 
             account1.ShowInfo();
             account2.ShowInfo();
--- Labs226-2021/Program.cs
+++ Labs226-2021/Program.cs
@@ -1,4 +1,4 @@
-﻿using Labs226_2021.Strategy;
+﻿
 using System;
 using System.Threading;
 
@@ -8,29 +8,7 @@ namespace Labs226_2021
     {
         static void Main(string[] args)
         {
-            BankAccount account1 = new BankAccount("Ivan", 5000, 1890);
-            BankAccount account2 = new BankAccount("Kirill", 3000, 1999);
-            BankAccount account3 = new BankAccount("Elena", 6000, 2000);
-
-            account1.ShowInfo();
-            account2.ShowInfo();
-            account3.ShowInfo();
-            BankAccount.rate = 0.004;
-            BankAccount.GetProfit(account1, 6);
-            BankAccount.GetProfit(account2, 6);
-            BankAccount.GetProfit(account3, 6);
-            account1.ShowInfo();
-            account2.ShowInfo();
-            account3.ShowInfo();
-            account1.Deposit(9);
-
-            account1.ShowInfo();
-            account1.Withdraw(5001);
-            account1.ShowInfo();
-            BankAccount.Transaction(account1, account2, 5000);
-            account1.ShowInfo();
-            account2.ShowInfo();
-
+            Account.Sber();
         }
     }
 }
