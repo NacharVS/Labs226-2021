ed5ee160c2657916708104975378aec0b351165a
Timghf <dinarhertz@gmail.com>
2021-12-01 13:59:55 +0300

    Create Security.cs

---
+++ Labs226-2021/Security.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Labs226_2021
+{
+    class Security : SberBank
+    {
+        public Security(string name, string surname, double balance) : base(name, surname, balance)
+        {
+        }
+
+        public void Sit(SberBank acc)
+        {
+            Console.WriteLine($"{acc.Name} cледит за порядком");
+        }
+
+        
+    }
+}
