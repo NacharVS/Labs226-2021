89c98bddba3f9c761840fc089d74fd948e1dc58c
Biburat123 <91465180+Biburat123@users.noreply.github.com>
2021-12-01 13:47:36 +0300

    w

---
+++ Labs/Client.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Labs226_2021
+{
+    class Client : BankAccount
+    {
+        public Client(string name, double balance, int yearofBirth) : base(name, balance, yearofBirth)
+        {
+
+        }
+        public override void Deposit(Client acc , double money)
+        {
+            if (Age > 0)
+            {
+                if (money < maxDeposit & money > minDeposit)
+                {
+                    Console.WriteLine($"Клиент {name} пришёл пополнить счёт в Банк");
+                    balance += money;
+                    Console.WriteLine($"Счёт пополнен на {money}");
+                    ShowInfo();
+
+                }
+                else
+                {
+                    System.Console.WriteLine("Лимит по депозиту");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Заблокировано");
+            }
+        }
+    }
+}
--- Labs/Program.cs
+++ Labs/Program.cs
@@ -1,12 +1,22 @@
-﻿using System;
+﻿using Labs226_2021.Strategy;
+using System;
+using System.Threading;
 
-namespace Labs
+namespace Labs226_2021
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Employee account1 = new Employee("Ivan", 0, 1999);
+            Client account2 = new Client("Kirill", 3000, 1999);
+            Client account3 = new Client("Kirilll", 70000, 1999);
+
+
+            account1.ShowInfo();
+            account2.ShowInfo();
+            account2.Deposit(account3 ,550);
+            account1.Deposit(account2, 1500);
         }
     }
 }
