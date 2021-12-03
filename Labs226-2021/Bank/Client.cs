923a5e6da0f716ac7d422038d9b9933431dafa18
DinarShigapov <dinarhertz@gmail.com>
2021-12-03 17:21:04 +0300

    bank

---
+++ Labs226-2021/Bank/Client.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Labs226_2021.Bank
+{
+    class Client : SberBank
+    {
+        public Client(string name, string surname, double balance) : base(name, surname, balance)
+        {
+        }
+
+        public override void Withdraw(SberBank acc, double money)
+        {
+            if (money > acc.MinWidtraw && money < acc.MaxWidtraw)
+            {
+
+                acc.Balance -= money;
+                Console.WriteLine($"{acc.Name} снять деньги на счёт");
+
+            }
+            else if (money <= acc.MinWidtraw)
+            {
+                Console.WriteLine("Минимальная сумма зачисления - 100");
+            }
+            else if (money >= acc.MaxWidtraw)
+            {
+                Console.WriteLine("Максимальная сумма зачисления  - 500000");
+            }
+        }
+
+        public override void ToDeposit(SberBank acc, double money)
+        {
+            if (money > acc.MinWidtraw && money < acc.MaxWidtraw)
+            {
+
+                acc.Balance += money;
+                Console.WriteLine($"{acc.Name} положил деньги на счёт");
+
+            }
+            else if (money <= acc.MinWidtraw)
+            {
+                Console.WriteLine("Минимальная сумма зачисления - 100");
+            }
+            else if (money >= acc.MaxWidtraw)
+            {
+                Console.WriteLine("Максимальная сумма зачисления  - 500000");
+            }
+        }
+
+        public override void Transaction(SberBank accSeller, SberBank accGetter, double money)
+        {
+            if (accSeller.Balance > money + (money * ComissionForTransaction))
+            {
+                accSeller.Balance -= money + money * ComissionForTransaction;
+                accGetter.Balance += money;
+            }
+            else
+            {
+                Console.WriteLine("Ошибка транзакции");
+            }
+        }
+
+
+
+
+
+    }
+}
---
+++ Labs226-2021/Bank/SberBank.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Labs226_2021.Bank
+{
+    class SberBank
+    {
+        private int age;
+        private int yearOfBirth;
+        private string name;
+        private string surname;
+        private double balance;
+
+        private static double rate = 0.007;
+        private static double minWidtraw = 100;
+        private static double maxWidtraw = 5000;
+        private static double minDeposit = 100;
+        private static double comissionForTransaction = 0.02;
+
+        public int Age { get => age; set => age = value; }
+        public string Name { get => name; set => name = value;}
+        public string Surname { get => surname; set => surname = value; }
+        public double Balance { get => balance; set => balance = value; }
+
+
+        public double Rate { get => rate; set => rate = value; }
+        public double MinWidtraw { get => minWidtraw; set => minWidtraw = value; }
+        public double MaxWidtraw { get => maxWidtraw; set => maxWidtraw = value; }
+        public double MinDeposit { get => minDeposit; set => minDeposit = value; }
+        public double ComissionForTransaction { get => comissionForTransaction; set => comissionForTransaction = value; }
+
+
+        public int YearOfBirth
+        {
+            get
+            {
+                return yearOfBirth;
+            }
+
+            set
+            {
+                if (value < 1900 || value >= DateTime.Now.Year)
+                {
+                    Console.WriteLine("Вы ввели неверный год рождения");
+                    Age = 0;
+                }
+                else
+                {
+                    Age = DateTime.Now.Year - value;
+                    Console.WriteLine($"Ваш возраст - {Age}");
+                }
+            }
+        }
+
+
+
+        public SberBank(string name, string surname , double balance)
+        {
+            this.Name = name;
+            this.Surname = surname;
+            this.Balance = balance;
+        }
+
+        public virtual void GoToBank()
+        {
+            Console.WriteLine($"{Name} goes to Bank");
+        }
+
+        public void ShowInfo()
+        {
+            System.Console.WriteLine($"{Name} - {Balance}");
+        }
+
+        public static void GetProfit(SberBank acc, int month)
+        {
+            for (int i = 0; i < month; i++)
+            {
+                acc.Balance += acc.Balance * acc.Rate;
+            }
+        }
+
+        public static void Deposit(double deposit, int month, SberBank acc)
+        {
+            if (deposit > minDeposit)
+            {
+                for (int i = 0; i < month; i++)
+                {
+
+                    deposit += deposit * 0.004;
+                }
+                acc.Balance += deposit;
+            }
+            else
+            {
+                Console.WriteLine("Минимальный депозит - 100");
+            }
+
+
+        }
+
+        public virtual void Withdraw(SberBank acc, double money)
+        {
+
+
+            if (money > acc.MinWidtraw && money < acc.MaxWidtraw)
+            {
+
+                acc.Balance -= money;
+
+            }
+            else if (money <= acc.MinWidtraw)
+            {
+                Console.WriteLine("Минимальный вывод - 100");
+            }
+            else if (money >= acc.MaxWidtraw)
+            {
+                Console.WriteLine("Максимальный вывод - 5000");
+            }
+
+
+        }
+
+        public virtual void ToDeposit(SberBank acc, double money)
+        {
+            if (money > acc.MinWidtraw && money < acc.MaxWidtraw)
+            {
+
+                acc.Balance += money;
+
+            }
+            else if (money <= acc.MinWidtraw)
+            {
+                Console.WriteLine("Минимальная сумма зачисления - 100");
+            }
+            else if (money >= acc.MaxWidtraw)
+            {
+                Console.WriteLine("Максимальная сумма зачисления  - 500000");
+            }
+        }
+
+
+
+        public virtual void Transaction(SberBank accSeller, SberBank accGetter, double money)
+        {
+            if (accSeller.Balance > money + (money * comissionForTransaction))
+            {
+                accSeller.Balance -= money + money * comissionForTransaction;
+                accGetter.Balance += money;
+            }
+            else
+            {
+                Console.WriteLine("Ошибка транзакции");
+            }
+        }
+    }
+}
