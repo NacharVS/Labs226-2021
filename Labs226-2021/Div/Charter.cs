d57fbf947c6be720aa448b68055c83aa5f31bb61
Biburat123 <90610603+NikitaMatv@users.noreply.github.com>
2021-12-04 14:39:10 +0300

    warior

---
+++ Labs226-2021/Div/Charter.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Labs226_2021.Div
+{
+   public class Charter 
+    {
+        public int _str;
+        public int _dex;
+        public int _int;
+        public int _con;
+
+        public int _mana;
+        public double _healing;
+        public double _damag;
+        public double _def;
+       
+    }
+   public class Warior : Charter
+    {
+        public int Str
+        {
+            get => _str;
+            set
+            {
+                if (value > 250 & value < 30)
+                {
+                    _str = value;
+                }
+                else
+                if(value < 30)
+                {
+                    _str = 30;
+
+                }
+                else
+                {
+                    _str = 250;
+                }
+            }
+        }
+        public int Dex
+        {
+            get => _dex;
+            set
+            {
+                if (value > 80 & value < 20)
+                {
+                    _dex = value;
+                }
+                else
+                if (value < 20)
+                {
+                    _dex = 20;
+
+                }
+                else
+                {
+                    _dex = 80;
+                }
+            }
+        }
+        public int Con
+        {
+            get => _con;
+            set
+            {
+                if (value > 100 & value < 25)
+                {
+                    _con = value;
+                }
+                else
+                if (value < 25)
+                {
+                    _con = 25;
+
+                }
+                else
+                {
+                    _con = 100;
+                }
+            }
+        }
+        public int Int
+        {
+            get => _int;
+            set
+            {
+                if (value > 50  & value < 10)
+                {
+                    _int = value;
+                }
+                else
+                if (value < 10)
+                {
+                    _int = 10;
+
+                }
+                else
+                {
+                    _int = 50;
+                }
+            }
+        }
+        public double Damag
+        {
+            get => _damag;
+            set
+            {
+                value = Str * 0.2 + Dex * 0.2;
+                _damag = value;
+            }
+
+        }
+        public double Defense
+        {
+            get => _def;
+            set
+            {
+                value = Dex * 0.1 + Con * 0.2;
+                _def = value;
+            }
+        }
+
+        public double Healing { get => _healing; set => _healing = 1.5 * Con + 0.5 * Str; }
+        public int Mana { get => _mana; set => _mana = Int * 2; }
+        
+        public  void Attack()
+        {
+
+            Console.WriteLine($"Юнит Wrior  нанёс урон  по маникену {Damag}");
+        }
+        public void ShowInfo() 
+        {
+           Console.WriteLine($" Str - {Str} Dex - {Dex} Con - {Con} Int - {Int} Damage - {Damag} Health - {Healing} Mana - {Mana} Def - {Defense}");
+       
+        }
+
+
+    }
+}
+
--- Labs226-2021/Program.cs
+++ Labs226-2021/Program.cs
@@ -1,6 +1,7 @@
 ﻿using Labs226_2021.Strategy;
 using System;
 using System.Threading;
+using Labs226_2021.Div;
 
 namespace Labs226_2021
 {
@@ -8,12 +9,17 @@ namespace Labs226_2021
     {
         static void Main(string[] args)
         {
-            IBankClient client1 = new BankAccount("Fedor",1 , 2000);
-            IBankClient client2 = new BankAccount("Rad", 2, 900);
-            IBankAdmin admin = new BankAccount("Anton", 0 ,8.79);
-            client1.ShowInfo();
-            client1.Trade(client1, client2, 1000);
-            admin.BankTrade( client1, client2 ,500);
+         Warior warior  = new Warior();
+            warior.Con = 7100;
+            warior.Dex = 80804;
+            warior.Str = 5045;
+            warior.Int = 1;
+            warior.Damag = 0;
+            warior.Mana = 0;
+            warior.Healing = 0;
+            warior.Defense = 0;
+            warior.ShowInfo();
+            warior.Attack();
 
         }
     }
