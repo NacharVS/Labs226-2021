78af3d4fd397d1ef3beafe73784e74287856a9c0
Biburat123 <90610603+NikitaMatv@users.noreply.github.com>
2021-12-16 12:33:17 +0300

    w

--- Labs/Program.cs
+++ Labs/Program.cs
@@ -1,22 +1,104 @@
-﻿using Labs226_2021.Strategy;
+﻿using Labs226_2021.Div;
 using System;
 using System.Threading;
 
+
+
 namespace Labs226_2021
-{
-    class Program
     {
-        static void Main(string[] args)
+        class Program
         {
-            Employee account1 = new Employee("Ivan", 0, 1999);
-            Client account2 = new Client("Kirill", 3000, 1999);
-            Client account3 = new Client("Kirilll", 70000, 1999);
+            static void Main(string[] args)
+            {
+                Warior warior = new Warior();
+                Mage mag = new Mage();
+                Rogur rogor = new Rogur();
+                warior.Con = 7100;
+                warior.Dex = 80804;
+                warior.Str = 5045;
+                warior.Int = 20;
+                warior.DamagP = 0;
+                warior.Mana = 0;
+                warior.Healing = 0;
+                warior.DefenseP = 0;
+                warior.Lvl = 0;
 
+                warior.ShowInfo();
+                warior.AttackP();
+                warior.Lvlup(10);
+                warior.ShowInfo();
+                warior.Lvlup(48);
+                warior.ShowInfo();
+                Console.WriteLine();
+                Console.WriteLine();
+                Console.WriteLine();
+                mag.Con = 7100;
+                mag.Dex = 80804;
+                mag.Str = 5045;
+                mag.Int = 1;
+                mag.DamagM = 0;
+                mag.Mana = 0;
+                mag.Healing = 0;
+                mag.DefenseM = 0;
+                mag.Lvl = 0;
+                rogor.Con = 7100;
+                rogor.Dex = 80804;
+                rogor.Str = 5045;
+                rogor.Int = 100;
+                rogor.DamagP = 0;
+                rogor.Mana = 35;
+                rogor.Healing = 0;
+                rogor.DefenseP = 0;
+                rogor.Lvl = 0;
 
-            account1.ShowInfo();
-            account2.ShowInfo();
-            account2.Deposit(account3 ,550);
-            account1.Deposit(account2, 1500);
+                mag.ShowInfo();
+                mag.AttackP();
+                mag.Lvlup(30);
+                mag.ShowInfo();
+                mag.Lvlup(78);
+                mag.ShowInfo();
+                Console.WriteLine();
+                Console.WriteLine("___");
+                Console.WriteLine();
+                rogor.ShowInfo();
+                Console.WriteLine();
+                Console.WriteLine("___");
+                Console.WriteLine();
+
+                rogor.Str = 1;
+                rogor.Con = 1;
+                rogor.Healing = 1;
+                rogor.FallMan();
+                rogor.FallHel();
+                Console.WriteLine();
+                Console.WriteLine("___");
+                Console.WriteLine();
+                rogor.ShowInfo();
+                Console.WriteLine();
+                Console.WriteLine("___");
+                Console.WriteLine(); Console.WriteLine();
+                Console.WriteLine("___");
+                Console.WriteLine(); Console.WriteLine();
+                Console.WriteLine("___");
+                Console.WriteLine();
+
+                rogor.AttackP();
+                rogor.Lvlup(5);
+                rogor.ShowInfo();
+                rogor.Lvlup(15);
+                rogor.ShowInfo();
+                Console.WriteLine();
+                Console.WriteLine();
+                Console.WriteLine();
+                rogor.Int = 10;
+                rogor.FallMan();
+                rogor.ShowInfo();
+
+            }
+
         }
     }
-}
+
+
+    
+
