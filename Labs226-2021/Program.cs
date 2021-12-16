8ec754b36303098e0760b50ced3ad9a3521254d2
Musisin228 <90610962+Musisin228@users.noreply.github.com>
2021-12-16 12:30:17 +0300

    .

--- Labs226-2021/Program.cs
+++ Labs226-2021/Program.cs
@@ -5,11 +5,25 @@
         static void Main(string[] args)
         {
             Warrior Voin = new Warrior();
-            Voin.Str = 150;
-            Voin.Dex = 30;
-            Voin.Int = 25;
-            Voin.Con = 60;
+            Archer Luchnik = new Archer();
+            Mage Mag = new Mage();
+            Voin._str = 150;
+            Voin._dex = 30;
+            Voin._int = 25;
+            Voin._con = 60;
             Voin.ShowInfo();
+            Luchnik._str = 150;
+            Luchnik._dex = 30;
+            Luchnik._int = 25;
+            Luchnik._con = 60;
+            Luchnik.ShowInfo();
+            Mag._str = 150;
+            Mag._dex = 30;
+            Mag._int = 25;
+            Mag._con = 60;
+            Mag.ShowInfo();
+            Mag.LvlUp();
+            Mag.ShowInfo();
         }
     }
 }
