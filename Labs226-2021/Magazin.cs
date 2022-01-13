4b5e956bd3e0914e3ea92e93911203d1ef1f86dd
PutinLopuh <90610785+PutinLopuh@users.noreply.github.com>
2022-01-13 16:50:17 +0300

    .

---
+++ Labs226-2021/Magazin.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Labs226_2021
+{
+    class Magazin
+    {
+        public string name;
+        public double price;
+        public int count;
+        public string manufacture;
+    }
+}
--- Labs226-2021/Program.cs
+++ Labs226-2021/Program.cs
@@ -4,26 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Warrior Voin = new Warrior();
-            Archer Luchnik = new Archer();
-            Mage Mag = new Mage();
-            Voin._str = 150;
-            Voin._dex = 30;
-            Voin._int = 25;
-            Voin._con = 60;
-            Voin.ShowInfo();
-            Luchnik._str = 33;
-            Luchnik._dex = 199;
-            Luchnik._int = 57;
-            Luchnik._con = 63;
-            Luchnik.ShowInfo();
-            Mag._str = 30;
-            Mag._dex = 60;
-            Mag._int = 214;
-            Mag._con = 50;
-            Mag.ShowInfo();
-            Mag.LvlUp();
-            Mag.ShowInfo();
+            Magazin Sigareti
         }
     }
 }
