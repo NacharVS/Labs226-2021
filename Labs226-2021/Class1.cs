d927cc11a2780d35ce043f8492c1595c85328de3
Samdan <sublosd69@gmail.com>
2022-01-17 15:10:54 +0300

    if (number < 0)              {                 Console.WriteLine("Ошибка!");                          }

---
+++ Labs226-2021/Class1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Labs226_2021.Shop
+{
+    class Product
+    {
+        public string name;
+        public double price;
+        public int number;
+        public string nameCreator;
+
+        
+        public Product(string name, double price, int number, string nameCreator)
+        {
+            this.name = name;
+            this.price = price;
+            this.number = number;
+            this.nameCreator = nameCreator;
+
+        }
+
+       public void SubNum()
+        {
+            int storage = int.Parse(Console.ReadLine());
+            number -= storage;
+
+        }
+        public void ShowInfo()
+        {
+           
+        }
+    }
+}
