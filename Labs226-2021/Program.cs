fef5bceeeffbe8167ae1bf3a641d4c8330043e40
Biburat123 <90610603+NikitaMatv@users.noreply.github.com>
2022-01-18 16:50:08 +0300

    w

--- Labs226-2021/Program.cs
+++ Labs226-2021/Program.cs
@@ -1,5 +1,6 @@
 ﻿using Labs226_2021.Div;
 using System;
+using Labs226_2021.shema;
 
 namespace Labs226_2021
 {
@@ -7,81 +8,85 @@ namespace Labs226_2021
     {
         static void Main(string[] args)
         {
-            Warior warior = new Warior();
+            Archer ark = new Archer();
+            Peasant pes = new Peasant();
             Mage mag = new Mage();
-            Rogur rogor = new Rogur();
-            //warior.Con = 7100;
-            //warior.Dex = 80804;
-            //warior.Str = 5045;
-            //warior.Int = 20;
+
+            //Warior warior = new Warior();
+            //Mage mag = new Mage();
+            //Rogur rogor = new Rogur();
+            ////warior.Con = 7100;
+            ////warior.Dex = 80804;
+            ////warior.Str = 5045;
+            ////warior.Int = 20;
 
 
-            //warior.ShowInfo();
-            //warior.AttackP();
-            //warior.Lvlup(10);
-            //warior.ShowInfo();
-            //warior.Lvlup(48);
-            //warior.ShowInfo();
-            //Console.WriteLine();
-            //Console.WriteLine();
-            //Console.WriteLine();
-            mag.Con = 7100;
-            mag.Dex = 80804;
-            mag.Str = 5045;
-            mag.Int = 1;
-            mag.Healing = 144;
-            //rogor.Con = 7100;
-            //rogor.Dex = 80804;
-            //rogor.Str = 5045;
-            //rogor.Int = 100;
-            //rogor.DamagP = 0;
-            //rogor.Mana = 35;
+            ////warior.ShowInfo();
+            ////warior.AttackP();
+            ////warior.Lvlup(10);
+            ////warior.ShowInfo();
+            ////warior.Lvlup(48);
+            ////warior.ShowInfo();
+            ////Console.WriteLine();
+            ////Console.WriteLine();
+            ////Console.WriteLine();
+            //mag.Con = 7100;
+            //mag.Dex = 80804;
+            //mag.Str = 5045;
+            //mag.Int = 1;
+            //mag.Healing = 144;
+            ////rogor.Con = 7100;
+            ////rogor.Dex = 80804;
+            ////rogor.Str = 5045;
+            ////rogor.Int = 100;
+            ////rogor.DamagP = 0;
+            ////rogor.Mana = 35;
 
 
-            mag.ShowInfo();
-            mag.AttackP();
-            mag.Lvlup(30);
-            mag.ShowInfo();
-            mag.Lvlup(78);
-            mag.ShowInfo();
-            mag.Prihod();
-            mag.ShowInfo();
-            //Console.WriteLine();
-            //Console.WriteLine("___");
-            //Console.WriteLine();
-            //rogor.ShowInfo();
-            //Console.WriteLine();
-            //Console.WriteLine("___");
-            //Console.WriteLine();
+            //mag.ShowInfo();
+            //mag.AttackP();
+            //mag.Lvlup(30);
+            //mag.ShowInfo();
+            //mag.Lvlup(78);
+            //mag.ShowInfo();
+            //mag.Prihod();
+            //mag.ShowInfo();
+            ////Console.WriteLine();
+            ////Console.WriteLine("___");
+            ////Console.WriteLine();
+            ////rogor.ShowInfo();
+            ////Console.WriteLine();
+            ////Console.WriteLine("___");
+            ////Console.WriteLine();
 
-            //rogor.Str = 1;
-            //rogor.Con = 1;
-            //rogor.Healing = 1;
-            //rogor.FallMan();
-            //rogor.FallHel();
-            //Console.WriteLine();
-            //Console.WriteLine("___");
-            //Console.WriteLine();
-            //rogor.ShowInfo();
-            //Console.WriteLine();
-            //Console.WriteLine("___");
-            //Console.WriteLine(); Console.WriteLine();
-            //Console.WriteLine("___");
-            //Console.WriteLine(); Console.WriteLine();
-            //Console.WriteLine("___");
-            //Console.WriteLine();
+            ////rogor.Str = 1;
+            ////rogor.Con = 1;
+            ////rogor.Healing = 1;
+            ////rogor.FallMan();
+            ////rogor.FallHel();
+            ////Console.WriteLine();
+            ////Console.WriteLine("___");
+            ////Console.WriteLine();
+            ////rogor.ShowInfo();
+            ////Console.WriteLine();
+            ////Console.WriteLine("___");
+            ////Console.WriteLine(); Console.WriteLine();
+            ////Console.WriteLine("___");
+            ////Console.WriteLine(); Console.WriteLine();
+            ////Console.WriteLine("___");
+            ////Console.WriteLine();
 
-            //rogor.AttackP();
-            //rogor.Lvlup(5);
-            //rogor.ShowInfo();
-            //rogor.Lvlup(15);
-            //rogor.ShowInfo();
-            //Console.WriteLine();
-            //Console.WriteLine();
-            //Console.WriteLine();
-            //rogor.Int = 10;
-            //rogor.FallMan();
-            //rogor.ShowInfo();
+            ////rogor.AttackP();
+            ////rogor.Lvlup(5);
+            ////rogor.ShowInfo();
+            ////rogor.Lvlup(15);
+            ////rogor.ShowInfo();
+            ////Console.WriteLine();
+            ////Console.WriteLine();
+            ////Console.WriteLine();
+            ////rogor.Int = 10;
+            ////rogor.FallMan();
+            ////rogor.ShowInfo();
 
         }
 
