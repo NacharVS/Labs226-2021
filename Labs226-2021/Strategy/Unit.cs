70aab618d9473d1be8879e8d64c45e69d8206c56
Musisin228 <90610962+Musisin228@users.noreply.github.com>
2022-01-24 14:51:02 +0300

    .

---
+++ Labs226-2021/Strategy/Unit.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Labs226_2021.Strategy
+{
+    class Unit
+    {
+        public string type;
+        public int health;
+        public int maxHealth;
+
+        public Unit(string type, int health)
+        {
+            this.type = type;
+            this.health = health;
+            this.maxHealth = health;
+        }
+        public void Creating()
+        {
+            System.Console.WriteLine($"Вы создали{type}");
+        }
+        public void UnitInfo()
+        {
+            System.Console.WriteLine($"Юнит: {type}. Здоровье: {health}.");
+        }
+        public virtual void Info()
+        {
+            Console.WriteLine($"Тип - {type}здоровье - {health} Максимальное здороье - {maxHealth}");
+        }
+        public void Termination()
+        {
+            System.Console.WriteLine($"{type} был(-а) уничтожен(-а)");
+        }
+    }
+}
