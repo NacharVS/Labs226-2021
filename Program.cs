d247b7154790aeea62f2de6814361165b97c6411
Tukfatullin <90610710+Tukfatullin@users.noreply.github.com>
2022-01-31 13:30:13 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,149 +1,24 @@
 ﻿using System;
-using System.Threading;
+using System.Collections.Generic;
+using System.Text;
 
-namespace ConsoleApp7
-
+namespace Labs226_2021.Shop
 {
-    class Program
+    class Product
     {
-        static void Main(string[] args)
-        {
-            BankAccount account1 = new BankAccount("Махмут Еврееич", 3000, 43);
-            BankAccount account2 = new BankAccount("Игорь Корявый", 100000, 20);
+        public string nameOfProduct;
+        public double price;
+        public int countOfProduct;
+        public string manufacturer;
+        public string counterparty;
 
-            account1.ShowInfo();
-            account2.ShowInfo();
-            BankAccount.GetProfit(account1, 2);
-            BankAccount.GetProfit(account2, 2);
-            account1.ShowInfo();
-            account2.ShowInfo();
-            BankAccount.Withdraw(account1, 100);
-            BankAccount.Deposit(account2, 5000);
-            BankAccount.Transaction(account1, account2);
-            account1.ShowInfo();
-            account2.ShowInfo();
+        public Product(string nameOfProduct, double price, int countOfProduct, string manufacturer, string conterparty)
+        {
+            this.nameOfProduct = nameOfProduct;
+            this.price = price;
+            this.countOfProduct = countOfProduct;
+            this.manufacturer = manufacturer;
+            this.counterparty = counterparty;
         }
     }
-    class BankAccount
-    {
-        public string name;
-        public double balance;
-        public int age;
-        public static double rate = 0.01;
-        public static double minWidtraw = 100;
-        public static double maxWidtraw = 5000;
-        public static double minDeposit = 10;
-        public static double comissionForTransaction = 0.05;
-
-
-
-        public BankAccount(string name, double balance, int age)
-        {
-            this.name = name;
-            this.balance = balance;
-            this.age = age;
-        }
-        public void ShowInfo()
-        {
-            if (age > 18)
-                System.Console.WriteLine($"{name} - {balance} - {age} годииков");
-            else
-            {
-                Console.WriteLine("Мал да удал");
-
-
-            }
-        }
-
-        public static void GetProfit(BankAccount acc, int month)
-        {
-            {
-
-                if (acc.age > 18)
-                {
-                    for (int i = 0; i < month; i++)
-                    {
-                        acc.balance += acc.balance * rate;
-                    }
-                }
-                else
-                {
-                    Console.WriteLine("Ты маленький, иди повзрослей");
-                }
-
-            }
-        }
-
-        public static void Deposit(BankAccount acc, double money)
-        {
-            if (acc.age > 18)
-            {
-                if (money < minDeposit)
-                {
-                    Console.WriteLine("Минимальный депозит составляет 10");
-                }
-                else
-                {
-                    acc.balance = acc.balance + money;
-                    Console.WriteLine($"На ваш баланс {acc.name} было зачисленно - {money}, теперь ваш баланс составляет - {acc.balance}");
-                }
-            }
-            else
-            {
-                Console.WriteLine("Ты маленький, иди повзрослей");
-            }
-
-        }
-
-        public static void Withdraw(BankAccount acc, double money)
-        {
-            if (money < minWidtraw)
-            {
-                Console.WriteLine($"Сумма, которую {acc.name} хочет вывести, слишком мала, можно вывести минимум {minWidtraw} ");
-            }
-            else if (money > maxWidtraw)
-            {
-                Console.WriteLine($"Сумма, которую {acc.name} хочет вывести, слишком велика, за раз он может вывести только {maxWidtraw} ");
-            }
-            else
-            {
-                acc.balance = acc.balance - money;
-                Console.WriteLine($"{acc.name} снял {money} на счету осталось {acc.balance}");
-            }
-
-        }
-
-        public static void Transaction(BankAccount accSeller, BankAccount accGetter)
-        {
-            if (accSeller.age > 18 & accGetter.age > 18)
-            {
-                Console.WriteLine($"Введите сколько хотите перевести");
-                double p = double.Parse(Console.ReadLine());
-
-                double k = (p * comissionForTransaction) + p;
-                if (k > accSeller.balance)
-                {
-                    Console.WriteLine("Денег нет, но вы держитесь");
-                }
-                else
-                {
-                    Console.WriteLine($"{accSeller.name}, Вы хотите совершить перевод {accGetter.name}?");
-                    string o = Console.ReadLine();
-                    if (o == "да")
-                    {
-
-                        accGetter.balance = accGetter.balance + p;
-                        Console.WriteLine($"С учётом коммисии с вашего счета вычлось {k}");
-                        accSeller.balance = accSeller.balance - k;
-                    }
-                }
-
-            }
-            else
-            {
-                Console.WriteLine("Ты маленький, иди повзрослей");
-            }
-        }
-    }
-
 }
---
+++ Store.cs
@@ -0,0 +1,71 @@
+﻿using Labs226_2021.Shop;
+using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace Labs226_2021
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product putter = new Product("Putter", 800.99, 10, "Larsen");
+            Product bread = new Product("Bread", 40, 180, "China");
+            Product shampoo = new Product("Shampoo", 200, 100, "Vietnam");
+            Product iphone = new Product("IPhone", 120000, 80, "USA");
+            Product potatos = new Product("potatos", 90, 80, "RT");
+            Product chakchak = new Product("чак-норис", 116, 16, "TARSTAN", "Яндекс Еда");
+            Product teate = new Product("Чай-норис", 611, 61, "NATSRAT", "Яндекс Еда");
+            Product mouse = new Product("Logitech", 650, 71, "CHINA", "Яндекс Маркет");
+            Product pencil = new Product("pencil", 40, 80, "RT");
+
+
+
+            List<Product> products = new List<Product>();
+            products.Add(putter);
+            products.Add(bread);
+            products.Add(shampoo);
+            products.Add(iphone);
+            products.Add(chakchak);
+            products.Add(teate);
+            products.Add(potatos);
+            products.Add(pencil);
+
+            foreach (var item in products)
+            {
+                //Console.WriteLine($" {item.nameOfProduct} - cost: " +
+                //    $"{item.countOfProduct * item.price}");
+
+                Console.WriteLine($"Name: {item.name}, Price:{item.price}, Count: {item.number}, Manufacturer: {item.nameCreator}");
+            }
+            putter.Bucket();
+            bread.Bucket();
+            shampoo.Bucket();
+            iphone.Bucket();
+
+
+            foreach (var item in products)
+            {
+                if (item.number < 0)
+                {
+                    Console.WriteLine("Ошибка!");
+                }
+                else
+                {
+
+
+
+                    Console.WriteLine($"Name: {item.name}, Price:{item.price}, Count: {item.number}, Manufacturer: {item.nameCreator}");
+                }
+
+            }
+
+
+        }
+
+        //static void ShowMessage(double balance, double old, double diff)
+        //{
+        //    Console.WriteLine($"Balance has changed! Old Balance {old} Current balance {balance}. Difference {diff}");
+        //}
+    }
+}
