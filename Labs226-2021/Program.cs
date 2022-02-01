a825c6435c8895b4ef761fd2d0cffbe8ea3dd18c
Timghf <tsharafutdinov300@gmail.com>
2022-02-01 15:34:55 +0300

    up

--- Labs226-2021/Program.cs
+++ Labs226-2021/Program.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using Labs226_2021.Interfaces;
+using Labs226_2021.Shop;
+using Labs226_2021.Strategy;
+using System;
+using System.Collections.Generic;
 using System.Threading;
 
 namespace Labs226_2021
@@ -7,7 +11,31 @@ namespace Labs226_2021
     {
         static void Main(string[] args)
         {
-            Shop.Store.Magaz();
+            Warrior Andrey = new Warrior(new Club());
+            Blacksmith Kuzma = new Blacksmith();
+            Andrey.Attack();
+            var weapon2 = Kuzma.CraftStoneAxe();
+            Andrey.ChangeWeapon(weapon2);
+            Andrey.Attack();
+            var weapon3 = Kuzma.CraftIronSword();
+            Andrey.ChangeWeapon(weapon3);
+            Andrey.Attack();
+            Console.WriteLine(weapon3.durability);
+            Kuzma.RepairItem(weapon3);
+            Console.WriteLine(weapon3.durability);
+            Kuzma.RepairItem(weapon3);
+            Console.WriteLine(weapon3.durability);
+            Kuzma.RepairItem(weapon2);
+            Console.WriteLine(weapon2.durability);
+
+
+
+
         }
+
+        //static void ShowMessage(double balance, double old, double diff)
+        //{
+        //    Console.WriteLine($"Balance has changed! Old Balance {old} Current balance {balance}. Difference {diff}");
+        //}
     }
 }
