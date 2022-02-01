6b21119b005084d49788d83446cad967ce4ea4b2
Timghf <tsharafutdinov300@gmail.com>
2022-02-01 15:37:10 +0300

    nb

--- Labs226-2021/Program.cs
+++ Labs226-2021/Program.cs
@@ -1,6 +1,8 @@
-﻿using Labs226_2021.Strategy;
+﻿using Labs226_2021.Interfaces;
 using Labs226_2021.Shop;
+using Labs226_2021.Strategy;
 using System;
+using System.Collections.Generic;
 using System.Threading;
 
 namespace Labs226_2021
@@ -9,7 +11,28 @@ namespace Labs226_2021
     {
         static void Main(string[] args)
         {
-            Strategy.Activate.Activ();
+            Warrior Andrey = new Warrior(new Club());
+            Blacksmith Kuzma = new Blacksmith();
+            Andrey.Attack();
+            var weapon2 = Kuzma.CraftStoneAxe();
+            Andrey.ChangeWeapon(weapon2);
+            Andrey.Attack();
+            var weapon3 = Kuzma.CraftIronSword();
+            Andrey.ChangeWeapon(weapon3);
+            Andrey.Attack();
+            Console.WriteLine(weapon3.durability);
+            Kuzma.RepairItem(weapon3);
+            Console.WriteLine(weapon3.durability);
+            Kuzma.RepairItem(weapon3);
+            Console.WriteLine(weapon3.durability);
+            Kuzma.RepairItem(weapon2);
+            Console.WriteLine(weapon2.durability);
+
+
+
+
         }
+
+
     }
 }
