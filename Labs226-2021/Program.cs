20e1600edc11fb932700a982dff59c904df1e7af
r3dinc <r3dinc@yandex.ru>
2022-02-01 15:49:39 +0300

    Revert "interfaces"
    This reverts commit 2747fd31c8670a5a37059b350bd34050e2d2a088.

--- Labs226-2021/Program.cs
+++ Labs226-2021/Program.cs
@@ -1,5 +1,4 @@
-﻿using Labs226_2021.Interfaces;
-using Labs226_2021.Shop;
+﻿using Labs226_2021.Shop;
 using Labs226_2021.Strategy;
 using System;
 using System.Collections.Generic;
@@ -11,24 +10,8 @@ namespace Labs226_2021
     {
         static void Main(string[] args)
         {
-            Warrior Andrey = new Warrior(new Club());
-            Blacksmith Kuzma = new Blacksmith();
-            Andrey.Attack();
-            var weapon2 = Kuzma.CraftStoneAxe();
-            Andrey.ChangeWeapon(weapon2);
-            Andrey.Attack();
-            var weapon3 = Kuzma.CraftIronSword();
-            Andrey.ChangeWeapon(weapon3);
-            Andrey.Attack();
-            Console.WriteLine(weapon3.durability);
-            Kuzma.RepairItem(weapon3);
-            Console.WriteLine(weapon3.durability);
-            Kuzma.RepairItem(weapon3);
-            Console.WriteLine(weapon3.durability);
-            Kuzma.RepairItem(weapon2);
-            Console.WriteLine(weapon2.durability);
-
-
+            Peasant pes = new Peasant();
+            pes.UnitInfo();
 
 
         }
