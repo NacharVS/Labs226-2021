861bb7533accb67bb1dafb4d3d6b69e47d3fef71
Timghf <tsharafutdinov300@gmail.com>
2022-02-10 16:01:39 +0300

    up

---
+++ Labs226-2021/MongoExamples/MongoExtensions.cs
@@ -0,0 +1,17 @@
+﻿
+using MongoDB.Driver;
+
+namespace Labs226_2021.MongoExamples
+{
+
+    class MongoExtensions
+    {
+        public static void AddToDataBase(User user)
+        {
+            var client = new MongoClient("mongodb://localhost");
+            var database = client.GetDatabase("226Group");
+            var collection = database.GetCollection<User>("Users");
+            collection.InsertOne(user);
+        }
+    }
+}
---
+++ Labs226-2021/MongoExamples/User.cs
@@ -0,0 +1,15 @@
+﻿namespace Labs226_2021.MongoExamples
+{
+    class User
+    {
+        public User(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+    }
+}
