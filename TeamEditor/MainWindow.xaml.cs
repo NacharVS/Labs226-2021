f9d44df850efced981979e568b5a4778942ff569
DinarShigapov <dinarhertz@gmail.com>
2022-03-03 12:01:06 +0300

    Update 1.1

--- TeamEditor/MainWindow.xaml.cs
+++ TeamEditor/MainWindow.xaml.cs
@@ -46,6 +46,8 @@ namespace TeamEditor
 
         private void bttAddToTeam_Click(object sender, RoutedEventArgs e)
         {
+            listOneTeam.Items.Clear();
+
             bool userBool = true;
             var selectUser = listUsers.SelectedItem.ToString();
             if (listOneTeam.Items.Count <= 5)
@@ -84,33 +86,56 @@ namespace TeamEditor
 
         private void bttRandomAddToTeam_Click(object sender, RoutedEventArgs e)
         {
+            listOneTeam.Items.Clear();
+            
             var randomUser = new Random();
             int[] array = new int[5];
             bool proverka = true;
-            
+            int k = 0;
+            int p = 0;
 
-            for (int i = 0; i < array.Length; i++)
+            while (k < listUsers.Items.Count)
             {
-                int buf = 0;
-                for (int k = 0; k < array.Length; k++)
+                p = randomUser.Next(listUsers.Items.Count);
+                bool b = true;
+                for (int i = 0; i < k; i++)
                 {
-                    buf = randomUser.Next(5);
-                    if (buf == array[k])
+                    if (p == array[i])
                     {
-                        proverka = false;
+                        b = false;
                         break;
                     }
-                    
-                    
                 }
-                if(proverka == true)
-                    array[i] = buf;
+
+                if (b)
+                {
+                    array[k] = p;
+                    k++;
+                }
+
+                
+            }
+            
+            bttRandomAddToTeam.Content = $"{array[0]}{array[1]}{array[2]}{array[3]}{array[4]}";
+
+            List<string> user = new List<string>();
+            user.Clear();
+            user.Add(listUsers.Items[array[0]].ToString());
+            user.Add(listUsers.Items[array[1]].ToString());
+            user.Add(listUsers.Items[array[2]].ToString());
+            user.Add(listUsers.Items[array[3]].ToString());
+            user.Add(listUsers.Items[array[4]].ToString());
+
+
+
+
+            foreach (var Item in user)
+            {
+                 listOneTeam.Items.Add(Item.ToString());
             }
 
-            bttRandomAddToTeam.Content =  $"{array[0]}{array[1]}{array[2]}{array[3]}{array[4]}";
-
-
-
+            
         }
+        
     }
 }
