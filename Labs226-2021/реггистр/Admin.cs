fa3608ff39025015ab2648e750f51a123fbc224b
Andrey36568 <53220738+Andrey36568@users.noreply.github.com>
2022-03-31 16:08:21 +0300

    .

---
+++ Labs226-2021/реггистр/Admin.cs
@@ -0,0 +1,14 @@
+﻿namespace Labs226_2021.MongoExamples
+{
+    class Admin
+    {
+        public Admin(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+}
---
+++ Labs226-2021/реггистр/MongoExtensions.cs
@@ -0,0 +1,17 @@
+﻿using MongoDB.Driver;
+
+namespace Labs226_2021.MongoExamples
+{
+    class MongoExtensions
+    {
+        public static void AddToDataBase(Admin admin)
+        {
+            var client = new MongoClient("mongodb://localhost");
+            var database = client.GetDatabase("226Group");
+            var collection = database.GetCollection<Admin>("Admins");
+            collection.InsertOne(admin);
+        }
+
+
+    }
+}
