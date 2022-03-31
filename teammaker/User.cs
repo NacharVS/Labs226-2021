41ecb6cff985e7eda91affb91938d7781ee8dadb
Tukfatullin <90610710+Tukfatullin@users.noreply.github.com>
2022-03-31 16:10:43 +0300

    l

---
+++ teammaker/User.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace teammaker
+{
+    class User
+    {
+
+        public User(string surname, string name, string patronymicu)
+        {
+
+            this.surname = surname;
+            this.name = name;
+            this.Patronymicu = patronymicu;
+        }
+
+        [BsonId]
+        [BsonIgnoreIfDefault]
+        public ObjectId _id { get; set; }
+        [BsonIgnoreIfDefault]
+        public string name { get; set; }
+        [BsonIgnoreIfDefault]
+        public string surname { get; set; }
+        [BsonIgnoreIfDefault]
+        public string Patronymicu { get; set; }
+
+    }
+}
