7bd22e48a532e10d2c42e3c7eeeaccfffe1923a1
Timghf <90610665+Timghf@users.noreply.github.com>
2022-03-31 15:46:16 +0300

    Update

---
+++ MatchMaker/User.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+
+namespace MatchMaker
+{
+    class User
+    {
+        public User(string surname, string name, string patronymicu)
+        {
+
+            this.surname = surname;
+            this.name = name;
+            this.Patronymicu = patronymicu;
+        }
+
+        [BsonId]
+        [BsonIgnoreIfDefault]
+        public ObjectId _id { get; set; }
+        [BsonIgnoreIfDefault]
+        public string name { get; set; }
+        [BsonIgnoreIfDefault]
+        public string surname { get; set; }
+        [BsonIgnoreIfDefault]
+        public string Patronymicu { get; set; }
+
+    }
+}
