60e3938c5a445602a0058cd3730bb52c737f14c6
Timghf <90610739+DinarShigapov@users.noreply.github.com>
2022-04-19 16:33:14 +0300

    up

---
+++ BlazorApp/Data/Task.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorApp.Data
+{
+    public class Task
+    {
+        private bool _inProgress;
+        private bool _isDone;
+        public string Name;
+
+        public bool InProgress
+        {
+            get { return _inProgress; }
+            set { _inProgress = value; }
+        }
+
+        public bool IsDone
+        {
+            get { return _inProgress; }
+            set { _inProgress = value; }
+        }
+
+
+    }
+}
