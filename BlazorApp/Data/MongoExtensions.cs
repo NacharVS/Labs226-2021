d3fb2eb5dea77db13043de1d223a2e3f9c6dbe4a
DinarShigapov <dinarhertz@gmail.com>
2022-04-24 20:20:50 +0300

    up

---
+++ BlazorApp/Data/MongoExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using MongoDB.Driver;
+using System.Collections.Generic;
+
+namespace BlazorApp.Data
+{
+    public class MongoExtensions
+    {
+
+        public static void AddItem(List<TaskListItem> user, string nameWeek)
+        {
+            var client = new MongoClient("mongodb://localhost");
+            var database = client.GetDatabase("TaskList");
+            var collection = database.GetCollection<TaskListItem>(nameWeek);
+
+            for (int i = 0; i < user.Count; i++)
+            {
+                TaskListItem taskBuf = new TaskListItem(string.Empty)
+                {
+                    Name = user[i].Name,
+                    IsDone = user[i].IsDone,
+                    InProgress = user[i].InProgress
+                };
+                collection.InsertOneAsync(taskBuf);
+            }
+        }
+    }
+}
---
+++ BlazorApp/Data/TaskListItem.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BlazorApp.Data
+{
+    public class TaskListItem
+    {
+        private bool _inProgress;
+        private bool _isDone;
+        private string _name;
+
+        public TaskListItem(string Name)
+        {
+            this.Name = Name;
+           
+        }
+
+
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+        public bool InProgress
+        {
+            get { return _inProgress; }
+            set { _inProgress = value; }
+        }
+
+        public bool IsDone
+        {
+            get { return _isDone; }
+            set { _isDone = value; }
+        }
+    }
+}
